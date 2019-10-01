4993c2d1e6fb07eb6fcde62a4fb919e8b7640c0d
doughalejr <55770931+doughalejr@users.noreply.github.com>
2019-10-01 08:36:05 -0400

    Add files via upload

--- camera.js
+++ camera.js
@@ -6,12 +6,11 @@ document.head.appendChild(imported);
 var constraints = { video: { facingMode: "environment" }, audio: false };
 // Define constants
 const cameraView = document.querySelector("#camera--view");
+const imageView = document.querySelector("#image--view"); //added by deh
 const cameraOutput = document.querySelector("#camera--output");
+const cameraOutput2 = document.querySelector("#camera--outputtofile"); //added by deh
 const cameraCanvas = document.querySelector("#camera--canvas");
 const cameraTrigger = document.querySelector("#camera--trigger");
-
-
-const imageView = document.querySelector("#image--view"); //added by deh
 
 // Access the device camera and stream to cameraView
 function cameraStart() {
@@ -28,18 +27,19 @@ function cameraStart() {
 
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function () {
-    //cameraCanvas.width = cameraView.videoWidth;
-    //cameraCanvas.height = cameraView.videoHeight;
-    //cameraCanvas.getContext("2d").drawImage(cameraView, 0, 0);
-	
-	cameraCanvas.width = imageView.width;//added by deh
-    cameraCanvas.height = imageView.height;//added by deh
-	cameraCanvas.getContext("2d").drawImage(imageView, 0, 0); //added by deh
-	
-    cameraOutput.src = cameraCanvas.toDataURL("image/webp");
+    cameraCanvas.width = cameraView.videoWidth;
+    cameraCanvas.height = cameraView.videoHeight;
+    cameraCanvas.getContext("2d").drawImage(cameraView, 0, 0);
+	//-------------------
+    //cameraCanvas.width = imageView.width;//added by deh
+    //cameraCanvas.height = imageView.height;//added by deh
+	//cameraCanvas.getContext("2d").drawImage(imageView, 0, 0); //added by deh
+	//-------------------
+    cameraOutput.src = cameraCanvas.toDataURL("image/jpg");
+    cameraOutput2.src = cameraCanvas.toDataURL("image/jpg");
+    cameraOutput.classList.add("taken");
 
-    var container = document.getElementById("camera--output"); //specific element on page
-    //var container = document.body; // full page 
+    var container = document.getElementById("camera--outputtofile"); //specific element on page
     try {
         html2canvas(container).then(function (canvas) {
             var link = document.createElement("a");
@@ -57,7 +57,6 @@ cameraTrigger.onclick = function () {
                 //window.URL.revokeObjectURL(url);
             }, 100); 
 
-			cameraOutput.classList.add("taken");
         });
     } catch (e) {
         console.log(e.message);
@@ -65,6 +64,5 @@ cameraTrigger.onclick = function () {
 
     //window.location.href('../Upload.aspx');
 };
-
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
